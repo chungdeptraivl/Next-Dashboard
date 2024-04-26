dfe3118958206a5ae7dbec4e673eeef0fdd081e0
chungdeptraivl <thanhchungrc229@gmail.com>
2024-04-26 16:18:00 +0700

    lan 2

--- app/ui/dashboard/invoices/status.tsx
+++ app/ui/dashboard/invoices/status.tsx
@@ -1,4 +1,4 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
+import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline'; // ./
 import clsx from 'clsx';
 
 export default function InvoiceStatus({ status }: { status: string }) {
@@ -7,7 +7,7 @@ export default function InvoiceStatus({ status }: { status: string }) {
       className={clsx(
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-gray-100 text-yellow-500': status === 'pending',
           'bg-green-500 text-white': status === 'paid',
         },
       )}
